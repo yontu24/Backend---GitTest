d0e636357d04045d310b63a0950fcb395e3686ad
CihoAlex <61946386+CihoAlex@users.noreply.github.com>
2020-04-01 11:50:15 +0300

    clasa Player
    Hei buna eu cu Andrei si Ionut am lucrat din greu, sper sa fie bine! :)

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace IP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Player Alex=new Player("Alex");
+            Console.WriteLine(Alex.DiceRoll());
+        }
+    }
+
+    class Player
+    {
+        public
+        string Nickname;
+        static int ID;
+        int skin;
+        int Points;
+        int RoadsLeft;
+        int VillagesLeft;
+        int PlayerType;
+        
+
+        public Player(string name)
+        {
+            Nickname = name;
+            ID++;
+
+        }
+
+        public int DiceRoll()
+        {
+            Random dice = new Random();
+            return dice.Next(1, 12);
+        }
+
+        public void PlayDevelopmentCard(int type)
+        {
+            //play(type);
+        }
+
+        public void EndTurn()
+        {
+            //player endTurn();
+        }
+
+        public void TradeWithPlayer(int id)
+        {
+            //trade(id);
+        }
+
+        public void TradeWithBand()
+        {
+            //TradeWithBand();
+        }
+
+        public bool BuyDevelopmentCard()
+        {
+            //check for resources;
+            //BuyDevelopmentCard();
+            return true;
+        }
+
+        public bool Build(int type)
+        {
+            //build(type);
+            return true;
+        }
+
+        public bool CheckForPort()
+        {
+            //ChekcForPort
+            //update player bank ratio
+            return true;
+        }
+
+        public bool TradeAccept()
+        {
+            return true;
+        }
+    }
+}
