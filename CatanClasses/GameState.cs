62778bd6a4fde17faee5339322779e73a2aa7b19
Scurtu Claudiu <37246308+isoon5@users.noreply.github.com>
2020-04-03 19:42:36 +0300

    Add files via upload
    private GameState

--- CatanClasses/GameState.cs
+++ CatanClasses/GameState.cs
@@ -6,16 +6,100 @@ namespace Catanv3
 {
     class GameState
     {
-        public Map map { get; set; }
-        public List<Player> players { get; set; }
+        private Map map = new Map();
 
-        public int knightCardsLeft { get; set; }
-        public int victoryPointCardsLeft { get; set; }
-        public int roadBuildingCardsLeft { get; set; }
-        public int yearOfPlentyCardsLeft { get; set; }
-        public int monopolyCardsLeft { get; set; }
+        public Map Map
+        {
+            get { return this.map; }
 
-        public Player longestRoadPlayer;
+        }
+        private List<Player> players = new List<Player>();
+        public List<Player> Players
+        {
+            get
+            {
+                return this.players;
+            }
+        }
+
+        private int knightCardsLeft;
+
+        public int KnightCardsLeft
+        {
+            get
+            {
+                return this.knightCardsLeft;
+            }
+            set
+            {
+                this.knightCardsLeft = value;
+            }
+        }
+
+        private int victoryPointCardsLeft;
+
+        public int VictoryPointsCardsLeft
+        {
+            get
+            {
+                return this.victoryPointCardsLeft;
+            }
+            set
+            {
+                this.victoryPointCardsLeft = value;
+            }
+        }
+
+        private int roadBuildingCardsLeft;
+
+        public int RoadBuildingCardsLeft
+        {
+            get
+            {
+                return this.roadBuildingCardsLeft;
+            }
+            set
+            {
+                this.roadBuildingCardsLeft = value;
+            }
+        }
+        private int yearOfPlentyCardsLeft;
+
+        public int YearOfPlentyCardsLeft
+        {
+            get
+            {
+                return this.yearOfPlentyCardsLeft;
+            }
+            set
+            {
+                this.yearOfPlentyCardsLeft = value;
+            }
+        }
+        private int monopolyCardsLeft;
+
+        public int MonopolyCardsLeft
+        {
+            get
+            {
+                return this.monopolyCardsLeft;
+            }
+            set
+            {
+                this.monopolyCardsLeft = value;
+            }
+        }
+
+        private Player longestRoadPlayer = new Player();
+        
+        public Player LongestRoadPlayer {
+
+            get
+            {
+                return this.longestRoadPlayer;
+            }
+
+        }
 
         public GameState(Map map, List<Player> players)
         {
