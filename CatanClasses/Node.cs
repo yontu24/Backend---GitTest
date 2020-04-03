1a7f6560767052fa19b46503ee48fe80503ece21
Scurtu Claudiu <37246308+isoon5@users.noreply.github.com>
2020-04-03 19:30:50 +0300

    Add files via upload
    private Node

--- CatanClasses/Node.cs
+++ CatanClasses/Node.cs
@@ -6,13 +6,53 @@ namespace Catanv3
 {
     class Node
     {
-        public int id { get; set; }
-        public List<Node> nodeNeighbors { get; set; }
-        public List<Node> hasRoadWith { get; set; }
-        public List<Hex> hexNeighbors { get; set; }
+        private int id;
+
+        public int Id {
+
+            get
+            {
+                return this.id;
+            }
+
+            set
+            {
+                this.id = value;
+            }
+
+        }
+        private List<Node> nodeNeighbors = new List<Node>();
+
+        public List<Node> NodeNeighbors{
+            get
+            {
+                return this.nodeNeighbors;
+            }
+        }
+
+        private List<Node> hasRoadWith = new List<Node>();
+
+        public List<Node> HasRoadWith
+        {
+            get
+            {
+                return this.hasRoadWith;
+
+            }
+        }
+
+        private List<Hex> hexNeighbors = new List<Hex>();
+
+        public List<Hex> HexNeighbors
+        {
+            get
+            {
+                return this.hexNeighbors;
+            }
+        }
 
         private bool hasPort;
-        /*public bool HasPort
+        public bool HasPort
         {
             get
             {
@@ -22,12 +62,40 @@ namespace Catanv3
             {
                 hasPort = value;
             }
-        }*/
-        public Resources portTradedResource { get; set; }
+        }
+        // private Resources portTradedResource { get; set; }
 
-        public bool hasSettlement { get; set; }
-        public Player playerSettled;
-        public Settlements settlementType { get; set; }
+        private bool hasSettlement;
+
+        public bool HasSettlement
+        {
+            get
+            {
+                return this.hasSettlement;
+            }
+            set
+            {
+                this.hasSettlement = value;
+            }
+        }
+        private Player playerSettled = new Player();
+        public Player PlayerSettled
+        {
+            get
+            {
+                return this.playerSettled;
+            }
+        }
+        private Settlements settlementType = new Settlements();
+
+        public Settlements SettlementType
+        {
+            get { 
+                return this.settlementType;
+            }
+        }
+
+        
 
 
 
