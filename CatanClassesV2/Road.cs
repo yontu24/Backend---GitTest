9b9496efbb77980d05cac470ba8beaaacb664935
IoanaParfene <57050677+IoanaParfene@users.noreply.github.com>
2020-04-04 14:28:48 +0300

    Update Road.cs

--- CatanClassesV2/Road.cs
+++ CatanClassesV2/Road.cs
@@ -7,12 +7,12 @@ namespace Catan
     class Road
     {
         private Node node1;
-        public Node node2;
+        private Node node2;
         private Player owner = new Player();
 
         // GETTERS AND SETTERS
         public Node Node1 { get{return this.node1;} set{this.node1 = value;} }
-        public Node Node2 { get{return this.node1;} set{this.node1 = value;} }
+        public Node Node2 { get{return this.node2;} set{this.node2 = value;} }
         public Player Owner { get { return this.owner;} set { this.owner = value; } }
 
     }
